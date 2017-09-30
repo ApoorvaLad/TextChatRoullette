582d2a9657924369ef303a25026c9b40cf770443
Neeraj Menon <nsmenon@users.noreply.github.com>
2017-09-29 20:04:54 -0400

    Update Chatclient.h

--- Chatclient.h
+++ Chatclient.h
@@ -20,20 +20,20 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define MSG_SIZE 1024
-#define COM_SIZE 30
-#define NAME_SIZE 30
+#define MAX_MSG_LEN 1024
+#define MAX_CMD_SIZE 20
+#define MAX_USERNAME_SIZE 20
 
 int fd_socket;
 int paired;
 int connection_active = 0;
 
-char username[NAME_SIZE];
+char username[MAX_USERNAME_SIZE];
 
 struct packet {
-    char username[NAME_SIZE];
-    char message[MSG_SIZE];
-    char clientCommand[COM_SIZE];
+    char username[MAX_USERNAME_SIZE];
+    char message[MAX_MSG_LEN];
+    char command[MAX_CMD_SIZE];
 };
 
 struct thread_data {
@@ -41,9 +41,8 @@ struct thread_data {
 };
 
 int createPacketForSending(const char *command, struct packet *send_packet);
-int sendPacketData(struct packet *packet);
 int serverConnect(char *port, struct addrinfo *hints, struct addrinfo **serverInfo,char *hostname, struct addrinfo **p);
-
+int sendPacketData(struct packet *packet);
 void *receiveMessage(void *argument);
 void handleReceivedMsg(struct packet *receivedMsg);
 
@@ -77,6 +76,7 @@ int serverConnect(char *port, struct addrinfo *hints, struct addrinfo **serverIn
     return 1;
 }
 
+
 void *receiveMessage(void *argument) {
 
     int isReceived;
@@ -108,111 +108,118 @@ void *receiveMessage(void *argument) {
 
 void handleReceivedMsg(struct packet *receivedMsg) {
 
-	if (strcmp((*receivedMsg).clientCommand, "ACKN") == 0
-			|| strcmp((*receivedMsg).clientCommand, "QUIT") == 0
-			|| strcmp((*receivedMsg).clientCommand, "HELP") == 0
-			|| strcmp((*receivedMsg).clientCommand, "SERVER_MSG") == 0) {
-		printf("%s\n", (*receivedMsg).message);
-	} else if (strcmp((*receivedMsg).clientCommand, "IN SESSION") == 0) {
-		printf("%s\n", (*receivedMsg).message);
-		paired = 1;
-	}
+    if (!strcmp((*receivedMsg).command, "ACKN")) {
+        printf("%s\n", (*receivedMsg).message);
+    }
+    else if (!strcmp((*receivedMsg).command, "IN SESSION")) {
+        printf("%s\n", (*receivedMsg).message);
+        paired = 1;
+    }
+    else if (!strcmp((*receivedMsg).command, "QUIT")) {
+        printf("%s\n", (*receivedMsg).message);
+    }
+    else if (!strcmp((*receivedMsg).command, "HELP")) {
+        printf("%s\n", (*receivedMsg).message);
+    }
+    else if (!strcmp((*receivedMsg).command, "TEXT")) {
+        printf("%s :::>>>> %s\n", (*receivedMsg).username, (*receivedMsg).message);
+    }
+    else if (!strcmp((*receivedMsg).command, "SERVER_MSG")) {
+        printf("%s\n", (*receivedMsg).message);
 
-	else if (strcmp((*receivedMsg).clientCommand, "TEXT") == 0) {
-		printf("%s :::>>>> %s\n", (*receivedMsg).username,
-				(*receivedMsg).message);
-	} else if (strcmp((*receivedMsg).clientCommand, "SERVER_THROWOUT") == 0) {
-		printf("%s\n", (*receivedMsg).message);
-		paired = 0;
-		if (paired == 0) {
-			printf("You are not paired with anyone");
+    }
+    else if (!strcmp((*receivedMsg).command, "SERVER_THROWOUT")) {
+        printf("%s\n", (*receivedMsg).message);
+        paired = 0;
+        if (paired == 0){
+            printf ("You are not paired with anyone");
+        }
+    }
+}
+
+int sendPacketData(struct packet *sendPacket) {
+	int sendPacketLen = sizeof *sendPacket;
+		int total = 0;
+		int countDataBytes = sendPacketLen;
+		int sentByteCounter, success;
+
+		while (total < sendPacketLen) {
+			sentByteCounter = send(fd_socket, (sendPacket + total), countDataBytes,
+					0);
+			;
+			if (sentByteCounter == -1) {
+				break;
+			}
+			total = total + sentByteCounter;
+			countDataBytes = countDataBytes - sentByteCounter;
 		}
-	}
+		memset(sendPacket, 0, sizeof(struct packet));
+		if (sentByteCounter == -1)
+			success = -1;
+		else
+			success = 0;
+		return success;
 }
 
 int createPacketForSending(const char *command, struct packet *send_packet) {
 
 	struct packet outgoing_packet;
-	strncpy(outgoing_packet.clientCommand, command, COM_SIZE);
-	strncpy(outgoing_packet.username, "username", NAME_SIZE);
-	strncpy(outgoing_packet.message, "message", MSG_SIZE);
+	strncpy(outgoing_packet.username, "username", MAX_USERNAME_SIZE);
+	strncpy(outgoing_packet.message, "message", MAX_MSG_LEN);
+	strncpy(outgoing_packet.command, command, MAX_CMD_SIZE);
 
-	if (strcmp(command, "CHAT") == 0) {
+		if (strcmp(command, "CHAT") == 0) {
 
-		char name_input[NAME_SIZE];
-		printf("Enter name: ");
-		fgets(name_input, sizeof name_input, stdin);
+			char name_input[MAX_USERNAME_SIZE];
+			printf("Enter name: ");
+			fgets(name_input, sizeof name_input, stdin);
 
-		int lenUsername = strlen(name_input);
+			int lenUsername = strlen(name_input);
 
-		if (name_input == NULL || strcmp(name_input, "\n") == 0
-				|| strcmp(name_input, " ") == 0) {
-			fprintf(stderr,
-					"Name cannot be blank or contain special characters.\n");
-			return -1;
-		}
-
-		if (lenUsername > 0 && name_input[lenUsername - 1] == '\n') {
-			name_input[lenUsername - 1] = '\0';
-		}
-
-		strncpy(outgoing_packet.username, name_input, NAME_SIZE);
-		strncpy(username, name_input, NAME_SIZE);
-
-		(*send_packet) = outgoing_packet;
-		return 1;
-	}
-
-	else if (strcmp(command, "TEXT") == 0) {
-		if (paired == 0) {
-			return -2;
-		} else {
-			fprintf(stdout, "YOU>>>>>>>> ");
-			fgets(outgoing_packet.message, sizeof outgoing_packet.message,
-					stdin);
-
-			int lenOutgoingPacket = strlen(outgoing_packet.message);
-			if (lenOutgoingPacket > 0
-					&& outgoing_packet.message[lenOutgoingPacket - 1] == '\n') {
-				outgoing_packet.message[lenOutgoingPacket - 1] = '\0';
+			if (name_input == NULL || strcmp(name_input, "\n") == 0
+					|| strcmp(name_input, " ") == 0) {
+				fprintf(stderr,
+						"Name cannot be blank or contain special characters.\n");
+				return -1;
 			}
 
-			strncpy(outgoing_packet.username, username, NAME_SIZE);
+			if (lenUsername > 0 && name_input[lenUsername - 1] == '\n') {
+				name_input[lenUsername - 1] = '\0';
+			}
+
+			strncpy(outgoing_packet.username, name_input, MAX_USERNAME_SIZE);
+			strncpy(username, name_input, MAX_USERNAME_SIZE);
+
 			(*send_packet) = outgoing_packet;
 			return 1;
 		}
-	} else if (strcmp(command, "QUIT") == 0) {
-		strncpy(outgoing_packet.username, username, NAME_SIZE);
-		(*send_packet) = outgoing_packet;
-		return 1;
-	} else if (strcmp(command, "CONNECT") == 0 || strcmp(command, "HELP") == 0
-			|| strcmp(command, "CONFIRM") == 0) {
-		(*send_packet) = outgoing_packet;
-		return 1;
-	}
-}
 
-int sendPacketData(struct packet *sendPacket) {
-	int sendPacketLen = sizeof *sendPacket;
-	int total = 0;
-	int countDataBytes = sendPacketLen;
-	int sentByteCounter, success;
+		else if (strcmp(command, "TEXT") == 0) {
+			if (paired == 0) {
+				return -2;
+			} else {
+				fprintf(stdout, "YOU>>>>>>>> ");
+				fgets(outgoing_packet.message, sizeof outgoing_packet.message,
+						stdin);
 
-	while (total < sendPacketLen) {
-		sentByteCounter = send(fd_socket, (sendPacket + total), sentByteCounter,
-				0);
-		;
-		if (sentByteCounter == -1) {
-			break;
+				int lenOutgoingPacket = strlen(outgoing_packet.message);
+				if (lenOutgoingPacket > 0
+						&& outgoing_packet.message[lenOutgoingPacket - 1] == '\n') {
+					outgoing_packet.message[lenOutgoingPacket - 1] = '\0';
+				}
+
+				strncpy(outgoing_packet.username, username, MAX_USERNAME_SIZE);
+				(*send_packet) = outgoing_packet;
+				return 1;
+			}
+		} else if (strcmp(command, "QUIT") == 0) {
+			strncpy(outgoing_packet.username, username, MAX_USERNAME_SIZE);
+			(*send_packet) = outgoing_packet;
+			return 1;
+		} else if (strcmp(command, "CONNECT") == 0 || strcmp(command, "HELP") == 0
+				|| strcmp(command, "CONFIRM") == 0) {
+			(*send_packet) = outgoing_packet;
+			return 1;
 		}
-		total = total + sentByteCounter;
-		countDataBytes = countDataBytes - sentByteCounter;
-	}
-	memset(sendPacket, 0, sizeof(struct packet));
-	if (sentByteCounter == -1)
-		success = -1;
-	else
-		success = 0;
-	return success;
 
 }
